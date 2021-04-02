afbdbe5fb4024249a7ca8e1f67bdee02445bf259
abc <def>
2021-04-01 19:49:34 -0700

    first commit

---
+++ src/main/java/Problem1.java
@@ -0,0 +1,19 @@
+public class Problem1 {
+
+    // The following variables are optional to use in your colorSort code. DO NOT MODIFY or REMOVE.
+    // The potential benefit of using these variable is making code easier to read comparing to using 0, 1, 2.
+    private static final int RED = 0;
+    private static final int WHITE = 1;
+    private static final int BLUE = 2;
+
+    /*
+    TODO: Describe your algorithm here in up to 5 sentences:
+     */
+
+    // Do not change signature (function name, parameter type, and return type)
+    public static void colorSort(int[] colors) {
+        // TODO: add your solution
+        // Do not use any sorting
+        // Your code needs to be 1-pass (if you have a nested loop, it's likely not 1-pass)
+    }
+}
